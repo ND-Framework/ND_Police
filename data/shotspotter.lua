e957d310747fa3a6436395b79ab15075a5c2b5fe
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2024-01-26 04:15:45 +0100

    tweak(data/shotspotter): locations & size

--- data/shotspotter.lua
+++ data/shotspotter.lua
@@ -3,23 +3,41 @@ local shotspotter = {}
 shotspotter.debug = false -- if you set it to true you will see the shotspotters on map.
 shotspotter.delay = 10 -- delay for shotspotter to report a shooting.
 shotspotter.cooldown = 120 -- seconds cooldown until player can trigger it again.
-shotspotter.radius = 450 -- how far the shotspotter will detect shots.
+shotspotter.radius = 550 -- how far the shotspotter will detect shots.
 
 shotspotter.ignoredJobs = {"sahp", "lspd", "bcso", "lsfd"}
 
 shotspotter.locations = {
-    vec3(653.4214, -648.7440, 57.1897),
-    vec3(1015.9837, -255.2573, 85.5857),
-    vec3(329.9973, 288.9604, 120.1029),
-    vec3(-202.7689, -327.3490, 66.0497),
-    vec3(31.3205, -875.2959, 31.4629),
+    vec3(653.42, -648.74, 57.18),
+    vec3(1015.98, -255.25, 85.58),
+    vec3(329.99, 288.96, 120.10),
+    vec3(-202.76, -327.34, 66.04),
+    vec3(31.32, -875.29, 31.46),
     vec3(70.1372, -1718.3291, 34.2056),
     vec3(1196.9178, -1624.6641, 50.3403),
     vec3(-852.9095, -1215.8782, 9.2463),
     vec3(-932.7648, -448.8844, 42.9436),
     vec3(-1713.6848, 478.4267, 130.3795),
     vec3(-596.5602, 515.0753, 109.675),
-    vec3(716.6274, -1958.7434, 44.7564)
+    vec3(716.6274, -1958.7434, 44.7564),
+    vec3(-1889.92, -351.62, 49.31),
+    vec3(-978.85, -2089.64, 10.19),
+    vec3(-1169.81, -2763.41, 13.95),
+    vec3(-96.09, 6377.95, 31.48),
+    vec3(1415.54, 2695.04, 37.42),
+    vec3(-3069.58, 735.29, 21.70),
+    vec3(-2729.48, 3.24, 15.51),
+    vec3(74.06, -2644.79, 21.90),
+    vec3(984.14, -3117.20, 5.90),
+    vec3(236.19, 1240.05, 229.83),
+    vec3(1806.03, 3705.84, 33.96),
+    vec3(1702.34, 4881.44, 42.03),
+    vec3(2868.15, 3567.91, 53.38),
+    vec3(2566.92, 383.00, 108.46),
+    vec3(1358.59, -942.04, 69.29),
+    vec3(-1591.69, -1140.81, 2.15),
+    vec3(-2380.90, 2136.90, 88.54),
+    vec3(375.02, 2778.28, 55.97)
 }
 
 -- weapons that won't trigger the shotspotter
